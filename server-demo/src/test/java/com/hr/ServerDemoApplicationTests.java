fa8cc4deae92104ca5c5de1030c5585ca460999f
cl900503 <1021773811@qq.com>
2018-07-18 21:13:33 +0800

    SpringBootTest运行成功

--- server-demo/src/test/java/com/hr/ServerDemoApplicationTests.java
+++ server-demo/src/test/java/com/hr/ServerDemoApplicationTests.java
@@ -2,16 +2,27 @@ package com.hr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hr.utils.OfficeUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ServerDemoApplicationTests {
-
+	
+	@Autowired
+	private OfficeUtil OfficeUtil;
+	
 	@Test
 	public void contextLoads() {
 		System.out.println("SpringBoot测试！！！！");
+	}
+	
+	@Test
+	public void testFf() {
+		OfficeUtil.ff("hello");
 	}
 
 }
