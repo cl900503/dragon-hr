0958f2f30124ead273b8a06e8aa68b0297033563
cl900503 <1021773811@qq.com>
2018-07-18 21:17:15 +0800

    office工具类

---
+++ server-demo/src/main/java/com/hr/utils/OfficeUtil.java
@@ -0,0 +1,21 @@
+package com.hr.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Office工具类
+ * @author chenlong
+ * @email 1021773811@qq.com
+ * @date 2018年7月18日下午9:02:24
+ *
+ */
+@Component
+public class OfficeUtil {
+	
+	
+	public void ff(String s) {
+		System.out.println("test方法执行："+s);
+	}
+	
+
+}
