cc39dabf96d4904d7e0d4204f3f53d6dd613fd64
cl900503 <1021773811@qq.com>
2018-07-25 16:21:29 +0800

    错误码

---
+++ server-demo/src/main/java/com/hr/constants/InterfaceErrorCode.java
@@ -0,0 +1,27 @@
+package com.hr.constants;
+
+/**
+ * server-demo服务接口错误码 错误码区间(1001000,1001500)
+ * @author chenlong
+ * @email 1021773811@qq.com
+ * @date 2018年7月25日下午4:08:01
+ *
+ */
+public class InterfaceErrorCode {
+	
+	/**
+	 * 成功
+	 */
+	public static final long SUCCESS = 0;
+	
+	/**
+	 * 失败
+	 */
+	public static final long FAIL = -1;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
+
